d23002d50196d7fdc384feb50431a8a6ab7f8b1f
Herrarte21368 <135072813+Herrarte21368@users.noreply.github.com>
2023-08-30 16:30:07 -0600

    Create config.h

---
+++ config.h
@@ -0,0 +1,9 @@
+#define IO_USERNAME  "Herrarte"
+#define IO_KEY       "aio_UZlq48CYAoA2peNZJQHfGJTN3LT3"
+
+#define WIFI_SSID "CHICKY"
+#define WIFI_PASS "joseluis1234"
+
+#include "AdafruitIO_WiFi.h"
+
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
